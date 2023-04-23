4aaad435e1c74a2214bfcfc89bb47b65f1781d3b
ramdev.io.id@gmail.com <ramdev.io.id@gmail.com>
2023-04-23 15:51:17 +0700

    test

---
+++ commands/general/test.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "test",
+  alias: ["tes", "tst"],
+  desc: ".",
+  category: "general",
+  async exec(RLink, m, { args, pushName, prefix, name }) {
+    
+    const input = args.split("|");
+    let input1 = input[0] // Nama pengirim 
+    let input2 = input[1] // Nomer penerima 
+    let input3 = input[2] // Pesan
+    
+    
+    RLink.sendMessage(`${input2}@s.whatsapp.net`, { text: `Kamu dapat pesan dari: ${input1}` })
+    RLink.sendMessage(`${input2}@s.whatsapp.net`, { text: `Pesan:\n${input3}` })
+  }
+}
+
